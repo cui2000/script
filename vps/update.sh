8f67d7046045a1534816a64e15869fbc0e863182
cui2000@163.com <cui_2000@163.com>
2024-05-18 11:02:36 +0800

    更新系统确认

--- vps/update.sh
+++ vps/update.sh
@@ -1,2 +1,11 @@
-yum clean all
-yum -y update
+echo "是否要更新系统："
+select opt in "是" "否"; do
+  case $opt in
+    "是")
+      yum clean all
+      yum -y update
+      yum -y install vim wget pcre pcre-devel zlib zlib-devel gcc gcc-c++ openssl openssl-devel automake autoconf libtool make
+      break
+      ;;
+  esac
+done
