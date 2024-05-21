79289b8ba4e3e053b957a8db60dbe40b0352fea6
cui2000@163.com <cui_2000@163.com>
2024-05-21 15:25:38 +0800

    修复更新不能选否的bug

--- vps/update.sh
+++ vps/update.sh
@@ -1,11 +1,14 @@
 echo "是否要更新系统："
 select opt in "是" "否"; do
   case $opt in
-    "是")
-      yum clean all
-      yum -y update
-      yum -y install vim wget pcre pcre-devel zlib zlib-devel gcc gcc-c++ openssl openssl-devel automake autoconf libtool make sssd
-      break
-      ;;
+  "是")
+    #yum clean all
+    yum -y update
+    yum -y install vim wget pcre pcre-devel zlib zlib-devel gcc gcc-c++ openssl openssl-devel automake autoconf libtool make sssd
+    break
+    ;;
+  "否")
+    break
+    ;;
   esac
 done
