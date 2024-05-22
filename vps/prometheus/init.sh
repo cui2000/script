2648806bc8a6f15ff929e153dbb7ea74ccc73234
cui2000@163.com <cui_2000@163.com>
2024-05-22 15:49:06 +0800

    修改创建用户的代码

--- vps/prometheus/init.sh
+++ vps/prometheus/init.sh
@@ -1,26 +1,6 @@
 #! /bin/bash
 # 此脚本用于初始化prometheus服务的用户和组
 
-function createUser() {
-  # 判断用户是否存在
-  username="prometheus"
-  if id -u "$username" >/dev/null 2>&1; then
-    echo "使用用户$username"
-  else
-    # 创建不能登录的用户
-    echo "创建用户$username"
-    useradd -s /sbin/nologin -M $username
-  fi
-
-  # 判断组是否存在
-  groupname="prometheus"
-  if getent group "$groupname" >/dev/null 2>&1; then
-    echo "使用用户组$groupname"
-  else
-    echo "创建用户组$groupname"
-    groupadd -r $groupname
-  fi
-}
 needInit="1"
 if [ ! -z "$1" ]; then
   needInit="$1"
@@ -30,7 +10,7 @@ if [ $needInit != "0" ]; then
   eval "$(curl -sL https://raw.githubusercontent.com/cui2000/script/dev/vps/update.sh)"
 
   # 创建用户
-  createUser
+  curl -sSL https://raw.githubusercontent.com/cui2000/script/dev/vps/createSystemUser.sh | bash -s "prometheus"
 
   # 建立文件夹
   mkdir -p /home/soft/prometheus/module
