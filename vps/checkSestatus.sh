9394e8c416e6cbebbef537dbafdf1971cfbb869b
cui2000@163.com <cui_2000@163.com>
2024-05-22 22:42:36 +0800

    检查SELinux模式

---
+++ vps/checkSestatus.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# 此脚本用于检查SELinux模式，某些应用在enforcing下会因为权限出问题
+
+# 查看SELinux模式，如果是enforcing，在运行中会因为权限出问题
+seStatus=$(sestatus | grep "Current mode" | awk '{print $3}')
+if [ "enforcing" = $seStatus ]; then
+  echo "请编辑/etc/selinux/config修改SELinux模式为permissive"
+  exit 0
+fi
