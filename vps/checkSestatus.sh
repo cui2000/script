eed4e3db90cc80503b416a1af2216958c5cedcfc
cui2000@163.com <cui_2000@163.com>
2024-05-22 23:19:45 +0800

    修复bug

--- vps/checkSestatus.sh
+++ vps/checkSestatus.sh
@@ -3,7 +3,7 @@
 
 # 查看SELinux模式，如果是enforcing，在运行中会因为权限出问题
 seStatus=$(sestatus | grep "Current mode" | awk '{print $3}')
-if [ "enforcing" = $seStatus ]; then
+if [ "$seStatus" = "enforcing" ]; then
   echo "请编辑/etc/selinux/config修改SELinux模式为permissive"
   exit 0
 fi
