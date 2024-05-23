8652c73c9a9a008a5d47f588f02fb82f82be1e2a
cui2000@163.com <cui_2000@163.com>
2024-05-23 10:49:07 +0800

    优化黑名单功能

--- vps/blackList2Firewalld.sh
+++ vps/blackList2Firewalld.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 # 此脚本用于firewalld服务，自动将超过10次失败的ip放入黑名单
 
+hasFirewalld=$(systemctl list-unit-files | grep firewalld)
+if [ -z "$hasFirewalld" ]; then
+  echo -n "firewalld服务未安装，是否安装？是（输入1），否（任意输入）："
+  read isInstall
+  if [ "$isInstall" = "1" ]; then
+    # 避免手误，关闭了ssh，安装防火墙后无法连接服务器
+    systemctl start sshd
+    # 安装firewalld
+    yum -y install firewalld
+    systemctl enable firewalld
+    systemctl start firewalld
+  else
+    exit 0
+  fi
+fi
+
 # 创建黑名单文件
 ipAccess="/home/ipAccess.list"
 ipBlack="/home/blackIp.txt"
@@ -21,12 +37,15 @@ fi
 
 # 统计超过10次登录失败的ip
 function setBlackList() {
-  echo "统计超过10次登录失败的ip并写入文件"
+  echo "统计超过10次登录失败的ip并写入文件$ipAccess"
   cat /var/log/secure | awk '/Failed/{print $(NF-3)}' | sort | uniq -c | awk '{print $2"="$1;}' >$ipAccess
   for i in $(cat $ipAccess); do
     IP=$(echo $i | awk -F= '{print $1}')
     NUM=$(echo $i | awk -F= '{print $2}')
-    echo $IP=$NUM
+    # 统计出来会有flush，原因待查
+    if [ "flush" = "$IP" ]; then
+        continue
+    fi
     if [ $NUM -gt 10 ]; then
       #grep $IP /etc/hosts.deny > /dev/null
       grep $IP $ipBlack >/dev/null
@@ -55,31 +74,44 @@ function addCrontab() {
   fi
 }
 
-# 设置黑名单
-isRunning=$(firewall-cmd --state)
-if [ "running" = "$isRunning" ]; then
+function installBlackList() {
   # 如果没有则创建
   hasBlackIpset=$(firewall-cmd --get-ipsets | grep $ipsetName)
   if [ -z "$hasBlackIpset" ]; then
-    echo "创建ipset：$ipsetName 用于黑名单"
     # 创建一个ipset
     # type选项中的hash:net对应的是ipv4的网络环境
     # 要创建用于IPv6的IP集，请添加--option = family = inet6选项
+    echo -n "创建ipset：$ipsetName 用于黑名单 "
     firewall-cmd --permanent --new-ipset=$ipsetName --type=hash:net
     # 在drop区域中定义一条源规则，将$ipsetName的地址集作为源规则的源IP
+    echo -n "将ipset：$ipsetName 添加到zone:drop "
     firewall-cmd --permanent --zone=drop --add-source=ipset:$ipsetName
     #firewall-cmd --permanent --zone=public --add-rich-rule="rule family='ipv4' source ipset=blacklist drop"
     #firewall-cmd --reload
   fi
   # 统计超过10次登录失败的ip并写入文件
   setBlackList
-  echo "更新黑名单"
+  echo -n "更新黑名单 "
   # 使用--add-entries-from-file选项将$ipBlack的内容导入到$ipsetName的ipset空间中
   firewall-cmd --permanent --ipset=$ipsetName --add-entries-from-file=$ipBlack
+  echo -n "刷新防火墙 "
   firewall-cmd --reload
 
   # 创建定时任务
   addCrontab
+}
+
+# 设置黑名单
+isRunning=$(firewall-cmd --state)
+if [ "running" = "$isRunning" ]; then
+  installBlackList
 else
-  echo "firewalld服务未启动，自动添加黑名单功能安装失败"
+  echo "firewalld服务未启动，已自动启动"
+  echo "为避免开启防火墙后导致服务不可用，请检查防火墙配置:firewall-cmd --list-all-zone"
+  # 避免手误，关闭了ssh，安装防火墙后无法连接服务器
+  systemctl start sshd
+  # 启动firewalld
+  systemctl enable firewalld
+  systemctl start firewalld
+  installBlackList
 fi
