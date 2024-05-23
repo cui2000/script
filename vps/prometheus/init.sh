9e91344006cd7757d52e74e9b12d99532f484bed
cui2000@163.com <cui_2000@163.com>
2024-05-23 20:44:25 +0800

    优化代码

--- vps/prometheus/init.sh
+++ vps/prometheus/init.sh
@@ -1,20 +1,13 @@
 #! /bin/bash
 # 此脚本用于初始化prometheus服务的用户和组
 
-needInit="$1"
-if [ -z "$needInit" ]; then
-  needInit="1"
-fi
-if [ $needInit = "1" ]; then
-  # 升级系统
-  eval "$(curl -sL https://raw.githubusercontent.com/cui2000/script/dev/vps/update.sh)"
-
-  # 创建用户
-  curl -sSL https://raw.githubusercontent.com/cui2000/script/dev/vps/createSystemUser.sh | bash -s "prometheus"
-
-  # 建立文件夹
-  mkdir -p /home/soft/prometheus/module
-
-  # 修改目录属主
-  chown -R prometheus:prometheus /home/soft/prometheus
-fi
+# 查看SELinux模式，如果是enforcing，在运行中会因为权限出问题
+eval "$(curl -sL https://raw.githubusercontent.com/cui2000/script/dev/vps/checkSestatus.sh)"
+# 升级系统
+eval "$(curl -sL https://raw.githubusercontent.com/cui2000/script/dev/vps/update.sh)"
+# 创建用户
+curl -sSL https://raw.githubusercontent.com/cui2000/script/dev/vps/createSystemUser.sh | bash -s "prometheus"
+# 建立文件夹
+mkdir -p /home/soft/prometheus/module
+# 修改目录属主
+chown -R prometheus:prometheus /home/soft/prometheus
