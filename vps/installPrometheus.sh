1823043a3925e9e2075d2f790c82d1cf46cc3c95
cui2000@163.com <cui_2000@163.com>
2024-05-23 09:53:17 +0800

    删除多余文件

--- vps/installPrometheus.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p /home/soft/prometheus
-cd /home/soft/
-wget https://github.com/prometheus/prometheus/releases/download/v2.45.5/prometheus-2.45.5.linux-amd64.tar.gz
-tar -zxvf prometheus-2.45.5.linux-amd64.tar.gz -C /home/soft/prometheus
