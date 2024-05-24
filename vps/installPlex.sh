cc5e494c1843056fb86610af135b81d3227d4d53
cui2000@163.com <cui_2000@163.com>
2024-05-24 22:39:51 +0800

    安装rclone,plex

---
+++ vps/installPlex.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# 此脚本用于安装plex服务
+
+hasPlex=$(systemctl list-unit-files | grep plexmediaserver)
+if [ -z "$hasPlex" ]; then
+  rootDir=/home/soft/plex
+  mkdir -p "$rootDir"
+  cd $rootDir
+  # 下载安装
+  curl -o plexmediaserver-1.40.2.8395-c67dce28e.x86_64.rpm https://downloads.plex.tv/plex-media-server-new/1.40.2.8395-c67dce28e/redhat/plexmediaserver-1.40.2.8395-c67dce28e.x86_64.rpm
+  yum -y install plexmediaserver-1.40.2.8395-c67dce28e.x86_64.rpm
+
+  # 等待安装完成，否则可能文件夹还没有建立
+  sleep 3
+  # 下载插件
+  wget -O XBMCnfoMoviesImporter.bundle.zip https://github.com/gboudreau/XBMCnfoMoviesImporter.bundle/archive/refs/heads/master.zip
+  unzip XBMCnfoMoviesImporter.bundle.zip
+  cp -rf XBMCnfoMoviesImporter.bundle-master /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/XBMCnfoMoviesImporter.bundle
+
+  wget -O JAVnfoMoviesImporter.bundle.zip https://github.com/ddd354/JAVnfoMoviesImporter.bundle/archive/refs/heads/master.zip
+  unzip JAVnfoMoviesImporter.bundle.zip
+  cp -rf JAVnfoMoviesImporter.bundle-master /var/lib/plexmediaserver/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/JAVnfoMoviesImporter.bundle
+
+  rm -rf XBMCnfoMoviesImporter*
+  rm -rf JAVnfoMoviesImporter*
+else
+  echo "plex已安装"
+fi
+# 开机启动
+systemctl enable plexmediaserver.service
+systemctl start plexmediaserver.service
+
+echo "第一次使用需要通过ssh转发端口，本地putty运行命令："
+echo "ssh root@服务器IP -L 8888:localhost:32400"
+echo "输入正确的密码后，再去浏览器里打开localhost:8888/web进入即可"
---
+++ vps/installRclone.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+# 此脚本用于安装plex服务
+
+hasRclone=$(systemctl list-unit-files | grep rclone)
+if [ -z "$hasRclone" ]; then
+  rootDir=/home/soft/rclone
+  dataDir=$rootDir/data/gdrive
+  cacheDir=$rootDir/data/cache
+  confDir=$rootDir/.config
+  # 建立文件夹
+  mkdir -p $confDir
+  mkdir -p $dataDir
+  mkdir -p $cacheDir
+  cd $rootDir
+  version=rclone-v1.66.0-linux-amd64
+#  eval "$(curl -sSL https://rclone.org/install.sh)"
+  wget -O $version.zip https://github.com/rclone/rclone/releases/download/v1.66.0/$version.zip
+  unzip $version.zip
+
+  #binary
+  mv $version/rclone /usr/bin/rclone
+  chmod 755 /usr/bin/rclone
+  chown root:root /usr/bin/rclone
+  #manual
+  if ! [ -x "$(command -v mandb)" ]; then
+    echo 'mandb not found. The rclone man docs will not be installed.'
+  else
+    mkdir -p /usr/local/share/man/man1
+    cp $version/rclone.1 /usr/local/share/man/man1/
+    mandb
+  fi
+  echo "[Unit]
+Description=Rclone service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/rclone mount --config $confDir/rclone.conf \\
+  --log-file $rootDir/rclone.log --transfers 32 gd: $dataDir \\
+  --copy-links --no-gzip-encoding --no-check-certificate \\
+  --allow-other --allow-non-empty --default-permissions --file-perms 0777 --umask 0000 \\
+  --vfs-cache-mode full --cache-dir $cacheDir \\
+  --vfs-cache-max-size 5G --buffer-size 64M --dir-cache-time 6h \\
+  --vfs-read-chunk-size 64M --vfs-read-chunk-size-limit 256M
+Restart=on-abort
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target" >/usr/lib/systemd/system/rclone.service
+
+else
+  echo "rclone已安装"
+fi
+#刷新
+systemctl daemon-reload
+# 开机启动
+systemctl enable rclone.service
+systemctl start rclone.service
+
+#update version variable post install
+version=$(rclone --version 2>>errors | head -n 1)
+
+#cleanup
+#rm -rf "$tmp_dir"
+
+printf "\n${version} has successfully installed."
+printf '\nNow run "rclone config" for setup. Check https://rclone.org/docs/ for more details.\n\n'
